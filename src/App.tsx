1e1a172aec733c16c7dcba9dae61c5d8079a0a6f
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-12-25 22:58:05 +0900

    AppコンポーネントにRowコンポーネントを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import React from "react";
+import { Row } from "./components/Row";
+import { requests } from "./request";
+
 function App() {
-  return <p>サンプルアプリを作成</p>;
+  return (
+    <div className="App">
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
+    </div>
+  );
 }
 
 export default App;
