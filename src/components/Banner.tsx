a8bb55bf1c0a98382ffb0d77d1760848dbd73f6d
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-12-26 22:40:31 +0900

    ランダムで画像を表示するバナーコンポーネントを追加

---
+++ src/components/Banner.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { requests } from "../request";
+import axios from "../axios";
+import "./Banner.scss";
+
+type movieProps = {
+  title?: string;
+  name?: string;
+  orignal_name?: string;
+  backdrop_path?: string;
+  overview?: string;
+};
+
+export const Banner = () => {
+  const [movie, setMovie] = useState<movieProps>({});
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.feachNetflixOriginals);
+
+      //apiから取得したデータをランダムに選択
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+
+  // descriptionの切り捨て
+  function truncate(str: any, n: number) {
+    if (str !== undefined) {
+      return str.length > n ? str?.substr(0, n - 1) + "..." : str;
+    }
+  }
+  return (
+    <header
+      className="Banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="Banner-contents">
+        <h1 className="banner-title">
+          {movie?.title || movie?.name || movie?.orignal_name}
+        </h1>
+        <div className="Banner-buttons">
+          <button className="Banner-button">Play</button>
+          <button className="Banner-button">My List</button>
+        </div>
+
+        <h1 className="Banner-description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+
+      <div className="Banner-fadeBottom" />
+    </header>
+  );
+};
